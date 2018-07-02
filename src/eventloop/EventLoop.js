efa486718dac2e6eed36857f5e8fee51a63697d1
Phoenixli <phoenixli@lagou.com>
2018-07-02 14:34:14 +0800

    增加router路由

---
+++ src/eventloop/EventLoop.js
@@ -0,0 +1,14 @@
+import React , { Component } from 'react';
+
+class EventLoop extends Component{
+
+    render(){
+        return (
+            <div>
+                EventLoop
+            </div>
+        )
+    }
+}
+
+export default EventLoop
--- src/index.js
+++ src/index.js
@@ -3,7 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import ClickCounter from './components/ClickCounter'
+import ClickCounter from './components/ClickCounter';
+import EventLoop from './eventloop/EventLoop';
 
-ReactDOM.render(<ClickCounter />, document.getElementById('root'));
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+
+
+const BasicRoute = () => (
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/clickcounter">ClickCounter</Link></li>
+                <li><Link to="/eventloop">EventLoop</Link></li>
+                <li><Link to="/app">App</Link></li>
+            </ul>
+
+            <hr/>
+
+            <Route exact path="/clickcounter" component={ ClickCounter }/>
+            <Route path="/eventloop" component={ EventLoop } />
+            <Route path="/app" component={ App }/>
+        </div>
+    </Router>
+)
+
+
+ReactDOM.render(<BasicRoute />, document.getElementById('root'));
 registerServiceWorker();
